ed522a28e6ae559306d76ef773b76e6ae5e7c6c3
Weezer <ewint3@gmail.com>
2019-10-29 17:43:53 -0500

    database setup

---
+++ db/seeds.sql
@@ -0,0 +1,3 @@
+INSERT INTO burgers (burger_name, devoured) VALUES ('Cheese Burger',0);
+INSERT INTO burgers (burger_name, devoured) VALUES ('Turkey Burger',0);
+INSERT INTO burgers (burger_name, devoured) VALUES ('Hamburger',0);
