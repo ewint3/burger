21c0bebb92480f824ca8d9661c98c3848959446b
Weezer <ewint3@gmail.com>
2019-10-29 18:44:45 -0500

    setup config

--- db/schema.sql
+++ db/schema.sql
@@ -4,9 +4,13 @@ CREATE DATABASE burgers_db;
 
 USE burgers_db;
 
+DROP DATABASE IF EXISTS burgers_db;
+USE burgers_db;
+
 CREATE TABLE burgers (
-  id int NOT NULL AUTO_INCREMENT,
-burger_name varchar(255) NOT NULL,
+  id INT (10) AUTO_INCREMENT NOT NULL,
+burger_name varchar(100) NOT NULL,
 devoured BOOLEAN,
+date TIMESTAMP,
   PRIMARY KEY (id)
 );
